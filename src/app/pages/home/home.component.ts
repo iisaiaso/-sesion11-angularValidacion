d6413c735b1fc8f520e1080a423719cd121abd79
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-04 14:29:13 -0500

    Agregando logica de validacion

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+
+
+
+export class HomeComponent implements OnInit {
+
+  public formLogin!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      name: ['',
+        [
+          Validators.required,
+          Validators.minLength(8)
+        ]
+      ],
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ]
+    })
+  }
+
+  send(): any {
+    console.log(this.formLogin.value)
+  }
 
 }
